fe2f6223cd5222544ea61ef0adeb138811746e2f
Jay Whitmore <jayraydon@gmail.com>
2022-12-26 14:36:05 +0700

    fix: add new template to index

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
     <LazyTemplate05 v-if="randomTemplate === 'template05'" />
     <LazyTemplate06 v-if="randomTemplate === 'template06'" />
     <LazyTemplate07 v-if="randomTemplate === 'template07'" />
+    <LazyTemplate08 v-if="randomTemplate === 'template08'" />
     <div :v-show="data.recipe" class="fixed bottom-0 z-20 flex items-center justify-center w-full py-4 text-sm text-center text-white bg-black md:text-base bg-opacity-90 backdrop-blur-md">
       <p>Try <a class="underline" :href="data.recipe.link" rel="noreferer">{{ data.recipe.recipeTitle }}</a> by {{ data.recipe.RecipeCreator }} on <a href="https://aeroprecipe.com">AeroPrecipe.com</a></p>
     </div>
@@ -23,7 +24,8 @@
         'template04',
         'template05',
         'template06',
-        'template07'
+        'template07',
+        'template08'
       ]
       return {
         data: {
