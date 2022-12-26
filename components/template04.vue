3318a19dd5ebdf4089726d719b54044f0f0ff37c
Jay Whitmore <jayraydon@gmail.com>
2022-12-26 13:27:46 +0700

    ...

--- components/template04.vue
+++ components/template04.vue
@@ -3,6 +3,6 @@
     class="flex flex-col items-center justify-center w-full h-full text-white bg-white"
     style="font-family: 'Courier New', Courier, monospace"
   >
-    <h1 class="z-10 text-lg text-black">yes.</h1>
+    <h1 class="z-10 text-lg text-center text-black">yes.</h1>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
     <LazyTemplate02 v-if="randomTemplate === 'template02'" />
     <LazyTemplate03 v-if="randomTemplate === 'template03'" />
     <LazyTemplate04 v-if="randomTemplate === 'template04'" />
+    <LazyTemplate05 v-if="randomTemplate === 'template05'" />
+    <LazyTemplate06 v-if="randomTemplate === 'template06'" />
+    <LazyTemplate07 v-if="randomTemplate === 'template07'" />
     <div :v-show="data.recipe" class="fixed bottom-0 z-20 flex items-center justify-center w-full py-4 text-white bg-black bg-opacity-90 backdrop-blur-md">
       <p>Try <a class="underline" :href="data.recipe.link" rel="noreferer">{{ data.recipe.recipeTitle }}</a> by {{ data.recipe.RecipeCreator }} on <a href="https://aeroprecipe.com">AeroPrecipe.com</a></p>
     </div>
@@ -17,7 +20,10 @@
         'template01',
         'template02',
         'template03',
-        'template04'
+        'template04',
+        'template05',
+        'template06',
+        'template07'
       ]
       return {
         data: {
